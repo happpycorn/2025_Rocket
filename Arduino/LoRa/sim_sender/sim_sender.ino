ee5c1dea92bb3962f84fef20c52345cee207b03b
corn <happpy.corn.man@gmail.com>
2025-04-16 21:59:32 +0800

    lora_update

--- Arduino/LoRa/sim_sender/sim_sender.ino
+++ Arduino/LoRa/sim_sender/sim_sender.ino
@@ -5,24 +5,26 @@
 
 HardwareSerial LoRa(1);  // 使用 UART1
 
-void setup() {
-    Serial.begin(115200);         // 給電腦看的 Serial
-    LoRa.begin(9600, SERIAL_8N1, 14, 13);  // RX=13, TX=14
+int send_count = 0;
 
+void setup() {
+    Serial.begin(115200);  
+    // 設置與 LoRa 模組通訊的串口，波特率需與模組相符（這裡用 9600 為例）
+    LoRa.begin(9600, SERIAL_8N1, 14, 13);  // RX=13, TX=14
+    
     // 設置 M0、M1 引腳為輸出並拉低，讓模組進入正常傳輸模式
     pinMode(M0_PIN, OUTPUT);
     pinMode(M1_PIN, OUTPUT);
     digitalWrite(M0_PIN, LOW);
     digitalWrite(M1_PIN, LOW);
-
+    
     delay(500);
-    Serial.println("LoRa Reciver Ready");
+    Serial.println("LoRa Sender Ready");
 }
 
 void loop() {
-    if (LoRa.available()) {
-        String received = LoRa.readStringUntil('\n');
-        Serial.print("Received: ");
-        Serial.println(received);
-    }
+    send_count++;
+    LoRa.println(send_count);
+    Serial.println("Sent: PING");
+    delay(1000);
 }
