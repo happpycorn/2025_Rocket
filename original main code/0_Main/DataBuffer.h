aff53da7a039de0fe9e7689ac73f94ebb5811334
fay.lin <fay.lin@yfy.com>
2025-07-07 14:32:47 +0800

    up

---
+++ original main code/0_Main/DataBuffer.h
@@ -0,0 +1,24 @@
+#ifndef DATABUFFER_H
+#define DATABUFFER_H
+
+#include <Arduino.h>
+#include <0_Config.h>
+
+class DataBuffer {
+private:
+
+    QueueHandle_t dataQueue; // 創建 Queue 變數
+    int queueLength;
+
+public:
+    // 建構子
+    DataBuffer(int length = 200);
+
+    // 儲存數據方法
+    void saveData(SensorData data);
+    bool getData(TotalData& data);
+    void createQueue();  // 創建 Queue 方法
+
+};
+
+#endif // DATABUFFER_H
