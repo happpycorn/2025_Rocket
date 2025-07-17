bf8b898ce3d2faf227f62f97db6945b99e9933ac
111flashlight <flashlight0526@gmail.com>
2025-07-17 12:22:29 +0800

    up

--- PlatfromIO_Rocket/src/Main_Test/Config.h
+++ PlatfromIO_Rocket/src/Main_Test/Config.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #pragma once
 
 #include <Wire.h>
@@ -6,8 +5,3 @@
 #include "Constant.h"
 #include "BMPController.h"
 #include "ServoController.h"
-=======
-#include "Constant.h"
-
-#include <Wire.h>
->>>>>>> 624a2f8dfb351222d41525585075789f354effe0
