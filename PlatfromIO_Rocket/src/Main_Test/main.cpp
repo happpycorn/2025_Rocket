ebdc5961fd40496c16c0bfc0913e182cb242a03b
111flashlight <flashlight0526@gmail.com>
2025-07-20 16:56:52 +0800

    servochange

--- PlatfromIO_Rocket/src/Main_Test/main.cpp
+++ PlatfromIO_Rocket/src/Main_Test/main.cpp
@@ -5,8 +5,8 @@ bool iicScan();
 void findiic(byte i, bool checklist[]);
 
 ServoController* servos[SERVO_COUNT] = {
-    new ServoController(SERVO_PIN_1, 90, 35, 1),
-    new ServoController(SERVO_PIN_2, 90, 35, 2),
+    new ServoController(SERVO_PIN_1, 35, 90, 1),
+    new ServoController(SERVO_PIN_2, 35, 90, 2),
     new ServoController(SERVO_PIN_3, 90, 35, 3)
 };
 
